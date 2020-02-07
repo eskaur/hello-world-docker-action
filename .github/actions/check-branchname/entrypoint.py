3fb7881533d18b76787b711120feea3d2c437e0e
Eskil Aursand <eskil.aursand@zivid.com>
2020-02-07 12:57:02 +0100

    WORK

--- .github/actions/check-branchname/entrypoint.py
+++ .github/actions/check-branchname/entrypoint.py
@@ -3,18 +3,13 @@
 import os
 import re
 
-
+# List of valid patterns
 valid_patterns = [
-    "^ZIVID-\d+-\w"
+    "^master$",
+    "^release-\d\.\d+\.\d+$",
+    "^ZIVID-\d+-\w",
+    "^MISC-\d{8}-\w"
 ]
-
-
-
-## Debug stuff
-#print("Environment variables: ")
-#for env in os.environ: 
-#    print("{0:40}:  {1}".format(env, os.getenv(env))) 
-
 
 # Get branch name
 GITHUB_REF = os.getenv("GITHUB_REF")
@@ -23,20 +18,20 @@ branch_name_prefix = "refs/heads/"
 assert GITHUB_REF.startswith(branch_name_prefix)
 branch_name = GITHUB_REF.split(branch_name_prefix)[-1]
 
-
-print(f"Checking branch name: {branch_name}")
+# Check branch name against patterns
+print(f"\nChecking branch name: {branch_name}")
 matches = 0
 for pattern in valid_patterns:
-    print(f"--- Pattern: {pattern}   ...", end="")
+    print(f"{'--- Pattern:':15} {pattern:30}   ...", end="")
 
     if re.match(pattern, branch_name):
         print("MATCH")
         matches += 1
     else:
-        print("NO MATCH")
+        print("NOT A MATCH")
 
 if matches > 0:
-    print(f"Branch name matched at least one pattern. ---OK")
+    print(f"\nBranch name matched at least one pattern. ---OK")
 else:
     raise AssertionError("Branch name did not match any of the patterns. ---FAILED")
 
