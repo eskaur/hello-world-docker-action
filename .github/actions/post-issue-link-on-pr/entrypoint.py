61e41b81b8748e0bea64961328baf7828b0f69c8
Eskil Aursand <eskil.aursand@zivid.com>
2020-02-07 14:20:33 +0100

    Tweak

--- .github/actions/post-issue-link-on-pr/entrypoint.py
+++ .github/actions/post-issue-link-on-pr/entrypoint.py
@@ -2,9 +2,38 @@
 
 import os
 import sys
+import re
 
 
-repo_token = sys.argv[1]
-print(f"Number of arguments: {len(sys.argv)}")
+
+print(os.environ)
+
 print(sys.argv)
-print(f"Repo token: {repo_token}")
+
+#repo_token = sys.argv[1]
+#print(f"Number of arguments: {len(sys.argv)}")
+#print(sys.argv)
+#print(f"Repo token: {repo_token}")
+#
+#
+#
+## Get branch name
+#GITHUB_REF = os.getenv("GITHUB_REF")
+#assert GITHUB_REF is not None
+#print(f"GITHUB_REF: {GITHUB_REF}")
+#
+
+
+#branch_name_prefix = "refs/heads/"
+#assert GITHUB_REF.startswith(branch_name_prefix)
+#branch_name = GITHUB_REF.split(branch_name_prefix)[-1]
+#
+## Get issue code
+#pattern = "^(ZIVID-\d+)-\w"
+#match = re.match(pattern, branch_name)
+#assert match is not None
+#issue_string = match.groups()[0]
+#
+#
+#print(f"Issue is: {issue_string}")
+#
