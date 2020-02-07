36c61bfbc735972835bb08a22808b45308a386e8
Eskil Aursand <eskil.aursand@zivid.com>
2020-02-07 11:09:48 +0100

    TEst

--- .github/actions/check-branchname/entrypoint.py
+++ .github/actions/check-branchname/entrypoint.py
@@ -4,6 +4,15 @@ import os
 
 print("Hello from Python")
 
+#print("Environment variables: ")
+#for env in os.environ: 
+#    print("{0:40}:  {1}".format(env, os.getenv(env))) 
 
-for env in os.environ: 
-    print("{0:40}:  {1}".format(env, os.getenv(env))) 
+
+
+GITHUB_REF = os.getenv("GITHUB_REF")
+assert GITHUB_REF is None
+
+
+
+
